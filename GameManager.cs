8518a83db7fbda3f95f530966378a4ecf59cb289
sleepysoul <sleepysoul84@gmail.com>
2023-08-15 02:51:01 +0900

    2023-08-14 Long Term Work
    - 게임플레이 UI 내 옵션 버튼 구현
    - 옵션 버튼 UI 구현
     - PLAY 버튼
     - RESET 버튼
     - EXIT 버튼
     - 스탯 초기화 버튼
    
    - 스탯 UI 구현
     - 현재 스테이지 표기
     - 총 점수 표기
     - 달러 표기
     - 총 플레이 타임 표기

--- GameManager.cs
+++ GameManager.cs
@@ -9,11 +9,14 @@ public class GameManager : MonoBehaviour
     [Header("===========[ Core ]")]
     public bool isOver;
     public bool isClear;
+    public bool isPause;
     public int minLevel;
     public int maxLevel;
     public int score;
     public int life;
+    public int dollar;
     public float playTime;
+    public float totalPlayTime;
 
     [Header("===========[ Obejct Pooling ]")]
     public Rigidbody2D hookRb;
@@ -44,18 +47,43 @@ public class GameManager : MonoBehaviour
     public enum Sfx { LevelUp, Next, GameOver, Attach, DongleAttach, Button, StageClear }
 
     [Header("===========[ UI ]")]
+    [Header("-----[ GamePlay UI ]")]
+    public GameObject touchPad;
     public Text stageText;
     public Text lifeText;
     public Text playTimeText;
     public Text scoreText;
-    public Text highScoreText;
-    public Text subScoreText;
-    public Text clearHighScoreText;
-    public Text clearSubScoreText;
-    public Image caution;    
+    public Image caution;
+    
+    [Header("-----[ GameOption UI ]")]
+    public GameObject optionUI;
+    public GameObject resetButtonSelectionUI;
+    public GameObject exitButtonSelectionUI;
+    public GameObject stageResetButtonSelectionUI;
+    public GameObject statsResetButtonSelectionUI;
+
+    [Header("-----[ GameOption Stats UI ]")]
+    public Text optionStageText;
+    public Text optionTotalScoreText;
+    public Text optionDollarText;
+    public Text optionTotalPlayTimeText;
+    public Text optionTotalMergeCountText;
+    public Text optionTotalGameOverCountText;
+
+    [Header("-----[ GameOver UI ]")]
     public GameObject endGroup;
+    public Text gameOverSubScoreText; // 현재 스테이지에서 얻은 점수
+    public Text overTotalScoreText;
+    public Text overDollarText;
+
+    [Header("-----[ GameClear UI ]")]
     public GameObject stageClearGroup;
-    public GameObject touchPad;
+    public Text stageClearSubScoreText; // 현재 스테이지에서 얻은 점수
+    public Text remainingLifeText;
+    public Text remainingPlayTimeText;
+    public Text stageClearScoreText;
+    public Text clearTotalScoreText;
+    public Text clearDollarText;
 
     [Header("===========[ ETC ]")]
     public GameObject line;
@@ -223,7 +251,8 @@ public class GameManager : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
         SfxPlay(Sfx.StageClear);
 
-        // 현재 게임 최종 스코어 출력
+        /* 현재 게임 최종 스코어 출력 */
+        // 남은 시간대 점수 환산
         int playTimeToScore;
         if (playTime < 60f) {
             playTimeToScore = 1;
@@ -232,16 +261,62 @@ public class GameManager : MonoBehaviour
             playTimeToScore = (int)(playTime / 60f);
         }
 
-        // 최종 스코어와 저장된 최고 스코어 비교하여 저장
+        // 현재 스테이지 점수 , 남은 라이프 , 남은 시간 , 계산된 점수 출력
+        Debug.Log("현재 스테이지 클리어 스코어 : " + score);
+        stageClearSubScoreText.text = "점수 : " + score.ToString();
+        Debug.Log("남은 라이프 : " + life);
+        remainingLifeText.text = "남은 라이프 보너스 : X " + life.ToString();
+        Debug.Log("남은 시간 : " + playTimeToScore);
+        remainingPlayTimeText.text = "남은 시간 보너스 : X " + playTimeToScore.ToString();
+        
         int stageClearScore = score * life * playTimeToScore;
-        clearSubScoreText.text = "SCORE : " + stageClearScore.ToString();
+        Debug.Log("합산 : " + stageClearScore);
+        stageClearScoreText.text = "합산 점수 : " + stageClearScore.ToString();
 
-        // 최고 스코어 출력
-        int highScore = Mathf.Max(stageClearScore, PlayerPrefs.GetInt("HighScore"));
-        PlayerPrefs.SetInt("HighScore", highScore);
-        clearHighScoreText.text = "HIGHSCORE : " + highScore.ToString();
+        /* 토탈 스코어에 저장 */
+        // 최초 득점 저장하기
+        if (!PlayerPrefs.HasKey("TotalScore"))
+        {
+            PlayerPrefs.SetInt("TotalScore", stageClearScore);
+            Debug.Log("최초 득점! Total Score 저장 완료 : " + stageClearScore);
+        }
+        else
+        {
+            // 저장 점수 + 클리어 시 점수 => 업데이트            
+            int savedTotalScore = PlayerPrefs.GetInt("TotalScore");
+            int upadteTotalScore = savedTotalScore + stageClearScore;
+            Debug.Log("저장된 Total Score : " + savedTotalScore);
+            Debug.Log("클리어 Score : " + stageClearScore);
+            Debug.Log("Total Score 저장 완료 : " + upadteTotalScore);
+            
+            PlayerPrefs.SetInt("TotalScore", upadteTotalScore);            
+        }
 
-        // 게임 오버 UI 출력
+        Debug.Log("총 점수 : " + PlayerPrefs.GetInt("TotalScore"));
+        clearTotalScoreText.text = "총 점수 : +" + stageClearScore.ToString() + "(" + PlayerPrefs.GetInt("TotalScore").ToString() + ")";
+
+        // 스테이지 합산 점수 / 10 = 달러 , 저장 및 출력
+        dollar = stageClearScore / 10;
+
+        // 첫 달러 획득 처리
+        if (!PlayerPrefs.HasKey("Dollar"))
+        {
+            PlayerPrefs.SetInt("Dollar", dollar);
+        }
+        // 저장된 Dollar 가 있다면 얻은 달러를 더해서 저장하기
+        else
+        {
+            int savedDollar = PlayerPrefs.GetInt("Dollar");
+            int updateDollar = savedDollar + dollar;
+            PlayerPrefs.SetInt("Dollar", updateDollar);
+        }
+        // 스테이지에서 얻은 달러 표기
+        clearDollarText.text = "+$" + dollar.ToString() + "($" + PlayerPrefs.GetInt("Dollar").ToString() + ")";
+
+        // 플레이 타임 저장
+        PlayTimeSave();
+
+        // 게임 클리어 UI 출력
         stageClearGroup.gameObject.SetActive(true);
 
         // BGM 종료
@@ -249,6 +324,7 @@ public class GameManager : MonoBehaviour
 
     }
 
+    // 스테이지 클리어 UI → 다음 스테이지 진행 버튼
     public void NextStageButton()
     {
         SfxPlay(Sfx.Button);
@@ -269,6 +345,7 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(sceneIndex + 1);
 
     }
+
 
     public void GameOver()
     {
@@ -298,14 +375,244 @@ public class GameManager : MonoBehaviour
         SfxPlay(Sfx.GameOver);
         bgmPlayer.Stop();
 
-        subScoreText.text = scoreText.text;
-        int highScore = Mathf.Max(score, PlayerPrefs.GetInt("HighScore"));
-        PlayerPrefs.SetInt("HighScore", highScore);
-        highScoreText.text = "HIGHSCORE : " + highScore.ToString();
+        // 게임오버 시 스테이지 점수 표기
+        gameOverSubScoreText.text = "점수 : " + score.ToString();
+
+        /* 토탈 스코어에 저장 */
+        // 최초 득점 저장하기
+        if (!PlayerPrefs.HasKey("TotalScore"))
+        {
+            PlayerPrefs.SetInt("TotalScore", score);
+            Debug.Log("최초 득점! Total Score 저장 완료 : " + score);
+        }
+        else
+        {
+            // 저장 점수 + 게임오버 시 점수 => 업데이트            
+            int savedTotalScore = PlayerPrefs.GetInt("TotalScore");
+            int upadteTotalScore = savedTotalScore + score;
+            Debug.Log("저장된 Total Score : " + savedTotalScore);
+            Debug.Log("게임오버 Score : " + score);
+            Debug.Log("Total Score 저장 완료 : " + upadteTotalScore);
+
+            PlayerPrefs.SetInt("TotalScore", upadteTotalScore);
+        }
 
         endGroup.gameObject.SetActive(true);
+
+        Debug.Log("총 점수 : " + PlayerPrefs.GetInt("TotalScore"));
+        overTotalScoreText.text = "총 점수 : +" + score.ToString() + "(" + PlayerPrefs.GetInt("TotalScore").ToString() + ")";
+
+        // 스테이지 합산 점수 / 10 = 달러 , 저장 및 출력
+        dollar = score / 10;
+
+        // 첫 달러 획득 처리
+        if (!PlayerPrefs.HasKey("Dollar"))
+        {
+            PlayerPrefs.SetInt("Dollar", dollar);
+        }
+        // 저장된 Dollar 가 있다면 얻은 달러를 더해서 저장하기
+        else
+        {
+            int savedDollar = PlayerPrefs.GetInt("Dollar");
+            int updateDollar = savedDollar + dollar;
+            PlayerPrefs.SetInt("Dollar", updateDollar);
+        }
+        // 스테이지에서 얻은 달러 표기
+        overDollarText.text = "+$" + dollar.ToString() + "($" + PlayerPrefs.GetInt("Dollar").ToString() + ")";
+
+        // 플레이 타임 저장
+        PlayTimeSave();
     }
 
+    // Option Button Pressed
+    public void OptionButtonPressed()
+    {
+        SfxPlay(Sfx.Attach);
+        isPause = true;
+
+        Invoke("OptionUI", 0.3f);
+    }
+
+    // Option UI
+    private void OptionUI()
+    {
+        optionUI.gameObject.SetActive(true);
+
+        /* Option UI > STATS TEXT 설정 */
+        // 스테이지 표기
+        optionStageText.text = stageText.text;
+
+        // 총 점수 표기
+        if (!PlayerPrefs.HasKey("TotalScore"))
+        {
+            PlayerPrefs.SetInt("TotalScore", 0);
+            optionTotalScoreText.text = "총 점수 : 0 (+" + score.ToString() + ")";
+        }
+        else
+        {
+            optionTotalScoreText.text = "총 점수 : " + PlayerPrefs.GetInt("TotalScore").ToString() + "(+" + score.ToString() + ")";
+        }        
+
+        // 달러 표기
+        if (!PlayerPrefs.HasKey("Dollar"))
+        {
+            PlayerPrefs.SetInt("Dollar", 0);
+            optionDollarText.text = "보유 금액 : $ 0";
+        }
+        else
+        {
+            optionDollarText.text = "보유 금액 : $ " + PlayerPrefs.GetInt("Dollar").ToString();
+        }
+
+        // 총 플레이타임 표기 => H:MM:SS
+        if (!PlayerPrefs.HasKey("PlayTime"))
+        {
+            PlayerPrefs.SetFloat("PlayTime", totalPlayTime);
+            optionTotalPlayTimeText.text = "총 플레이 타임 : 00:00:00";
+        }
+        else
+        {
+            float totalSeconds = PlayerPrefs.GetFloat("PlayTime");
+            int hours = Mathf.FloorToInt(totalSeconds / 3600);
+            int minutes = Mathf.FloorToInt((totalSeconds % 3600) / 60);
+            int seconds = Mathf.FloorToInt(totalSeconds % 60);
+
+            string formattedTime = string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
+            optionTotalPlayTimeText.text = formattedTime;
+        }        
+
+        // 총 머지 횟수
+
+        // 총 게임오버 횟수
+
+    }
+
+
+    // Option UI > PLAY Button
+    public void OptionPlayButtonPressed()
+    {
+        SfxPlay(Sfx.Attach);
+        isPause = false;
+        optionUI.gameObject.SetActive(false);
+    }
+
+    // Option UI > STAGE RESET Button
+    public void StageResetButtonPressed()
+    {
+        SfxPlay(Sfx.Attach);
+
+        Invoke("StageResetButtonSelectionUI", 0.2f);
+
+    }
+
+    private void StageResetButtonSelectionUI()
+    {
+        stageResetButtonSelectionUI.gameObject.SetActive(true);
+    }
+
+    public void StageResetButtonSelectionUIYesButton()
+    {
+        SfxPlay(Sfx.Attach);
+        SceneManager.LoadScene(1);
+    }
+
+    public void StageResetButtonSelectionUINoButton()
+    {
+        SfxPlay(Sfx.Attach);
+        stageResetButtonSelectionUI.gameObject.SetActive(false);
+    }
+
+    // Option UI > STATS RESET Button
+    public void StatsResetButtonPressed()
+    {
+        SfxPlay(Sfx.Attach);
+
+        Invoke("StatsResetButtonSelectionUI", 0.2f);
+
+    }
+
+    private void StatsResetButtonSelectionUI()
+    {
+        statsResetButtonSelectionUI.gameObject.SetActive(true);
+    }
+
+    public void StatsResetButtonSelectionUIYesButton()
+    {
+        SfxPlay(Sfx.Attach);
+
+        // 모든 정보 초기화
+
+
+        // 초기화 완료 후 선택 UI 비활성화
+        statsResetButtonSelectionUI.gameObject.SetActive(false);
+    }
+
+    public void StatsResetButtonSelectionUINoButton()
+    {
+        SfxPlay(Sfx.Attach);
+        statsResetButtonSelectionUI.gameObject.SetActive(false);
+    }
+
+    // Option UI > RESET Button
+    public void OptionResetButtonPressed()
+    {
+        SfxPlay(Sfx.Attach);
+
+        Invoke("ResetButtonSelectionUI", 0.3f);
+        
+    }
+    
+    private void ResetButtonSelectionUI()
+    {
+        resetButtonSelectionUI.gameObject.SetActive(true);
+    }
+
+    public void ResetButtonSelectionUIYesButton()
+    {
+        SfxPlay(Sfx.Attach);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ResetButtonSelectionUINoButton()
+    {
+        SfxPlay(Sfx.Attach);
+        resetButtonSelectionUI.gameObject.SetActive(false);
+    }
+
+
+    // Option UI > EXIT Button
+    public void OptionExitButtonPressed()
+    {
+        SfxPlay(Sfx.Attach);
+
+        Invoke("ExitButtonSelectionUI", 0.3f);
+
+    }
+
+    private void ExitButtonSelectionUI()
+    {
+        exitButtonSelectionUI.gameObject.SetActive(true);
+    }
+
+    public void ExitButtonSelectionUIYesButton()
+    {
+        SfxPlay(Sfx.Attach);
+
+        // 플레이 정보 저장 함수 호출
+
+        // 게임 종료 → 메인으로 이동
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitButtonSelectionUINoButton()
+    {
+        SfxPlay(Sfx.Attach);
+        exitButtonSelectionUI.gameObject.SetActive(false);
+    }
+
+
+
+    // 개발자 모드 버튼
     public void Reset()
     {
         SfxPlay(Sfx.Button);
@@ -326,23 +633,121 @@ public class GameManager : MonoBehaviour
 
         StartCoroutine(TestNextStageButtonRoutine());
     }
-
+    
     IEnumerator TestNextStageButtonRoutine()
     {
         yield return new WaitForSeconds(0.5f);
 
         int sceneIndex = SceneManager.GetActiveScene().buildIndex;
 
-        if (sceneIndex > SceneManager.sceneCount) {
+        if (sceneIndex > SceneManager.sceneCount)
+        {
             SceneManager.LoadScene(1);
         }
 
         SceneManager.LoadScene(sceneIndex + 1);
     }
 
+    private void PlayTimeSave()
+    {
+        if (!PlayerPrefs.HasKey("PlayTime"))
+        {
+            Debug.Log("저장된 플레이타임이 없습니다.");
+            PlayerPrefs.SetFloat("PlayTime", totalPlayTime);
+        }
+        else
+        {
+            float savedTotalPlayTime = PlayerPrefs.GetFloat("PlayTime");
+            float updatePlayTime = savedTotalPlayTime + totalPlayTime;
+            PlayerPrefs.SetFloat("PlayTime", updatePlayTime);
+        }
+    }
+
+    void Update()
+    {
+        PlayTimeCheck();
+    }
+
+    public void PlayTimeCheck()
+    {
+        if (isOver || isClear || isPause)
+        {
+            return;
+        }
+
+        int min = (int)playTime / 60;
+        float sec = playTime % 60;
+        playTime -= Time.deltaTime;
+        totalPlayTime += Time.deltaTime;
+
+        if (playTime >= 60f)
+        {
+
+            playTimeText.text = "0" + min + " : " + (int)sec;
+            if ((int)sec < 10)
+            {
+                playTimeText.text = "0" + min + " : " + "0" + (int)sec;
+            }
+        }
+
+        if (playTime < 60f)
+        {
+            playTimeText.text = "00 : " + (int)playTime;
+            if ((int)sec < 10)
+            {
+                playTimeText.text = "0" + min + " : " + "0" + (int)sec;
+            }
+        }
+
+        if (playTime <= 0)
+        {
+            playTimeText.text = "00 : 00";
+        }
+    }
+
+    void LateUpdate()
+    {
+        scoreText.text = "점수 : " + score.ToString();
+        lifeText.text = "X " + life.ToString();
+
+        if (maxLevel == 6)
+        {
+            if (isClear)
+            {
+                return;
+            }
+
+            isClear = true;
+            StageClear();
+        }
+    }
+
+    // 갑작스러운 게임 종료 시, 진행 정보 저장
+    private void OnApplicationQuit()
+    {        
+        // 옵션 UI 를 활성화 (pause)
+        Debug.Log("종료 감지. 옵션 UI 를 활성화합니다.");
+        optionUI.gameObject.SetActive(true);
+        // 진행중이던 현재 스테이지 PlayerPrefs 저장
+        Debug.Log("종료 감지. 현재 스테이지 [ " + SceneManager.GetActiveScene().name + " ] 정보를 저장합니다.");
+        PlayerPrefs.SetString("Stage", SceneManager.GetActiveScene().name);
+        // 플레이 타임 저장
+        Debug.Log("종료 감지. 플레이타임 (" + totalPlayTime + ") 을 저장합니다.");
+        if (!PlayerPrefs.HasKey("PlayTime"))
+        {
+            Debug.Log("저장된 플레이타임이 없습니다.");
+        }
+        else
+        {
+            PlayTimeSave();
+        }
+    }
+
+    // SFX
     public void SfxPlay(Sfx type)
     {
-        switch (type) {
+        switch (type)
+        {
             case Sfx.LevelUp:
                 sfxPlayer[sfxCursor].clip = sfxClip[Random.Range(0, 3)];
                 break;
@@ -354,7 +759,7 @@ public class GameManager : MonoBehaviour
                 break;
             case Sfx.Attach:
                 sfxPlayer[sfxCursor].clip = sfxClip[5];
-                break;            
+                break;
             case Sfx.DongleAttach:
                 sfxPlayer[sfxCursor].clip = sfxClip[6];
                 break;
@@ -368,62 +773,5 @@ public class GameManager : MonoBehaviour
 
         sfxPlayer[sfxCursor].Play();
         sfxCursor = (sfxCursor + 1) % sfxPlayer.Length;
-    }
-
-    void Update()
-    {
-        PlayTimeCheck();
-    }
-
-    public void PlayTimeCheck()
-    {
-        if (isOver || isClear) {
-            return;
-        }
-
-        int min = (int)playTime / 60;
-        float sec = playTime % 60;
-        playTime -= Time.deltaTime;
-
-        if (playTime >= 60f) {
-
-            playTimeText.text = "0" + min + " : " + (int)sec;
-            if ((int)sec < 10) {
-                playTimeText.text = "0" + min + " : " + "0" + (int)sec;
-            }
-        }
-
-        if (playTime < 60f) {
-            playTimeText.text = "00 : " + (int)playTime;
-            if ((int)sec < 10) {
-                playTimeText.text = "0" + min + " : " + "0" + (int)sec;
-            }
-        }
-
-        if (playTime <= 0) {
-            playTimeText.text = "00 : 00";
-        }
-    }
-
-    void LateUpdate()
-    {
-        scoreText.text = "SCORE : " + score.ToString();
-        lifeText.text = "X " + life.ToString();
-
-        if (maxLevel == 6) {
-            if (isClear) {
-                return;
-            }
-
-            isClear = true;
-            StageClear();
-        }
-    }
-
-    private void OnApplicationQuit()
-    {
-        // 게임 종료 시, 진행중이던 현재 스테이지 PlayerPrefs 저장
-        Debug.Log("종료 감지. 현재 스테이지 [ " + SceneManager.GetActiveScene().name + " ] 정보를 저장합니다.");
-        PlayerPrefs.SetString("Stage", SceneManager.GetActiveScene().name);
     }
 }
